97f13ef132ebc281b8dd183049a13c0b6bd1555c
Shafin <shafinhasnat97@gmail.com>
2021-06-08 21:48:05 +0600

    weatherdata

--- lambda/lambdas/weatherData.js
+++ lambda/lambdas/weatherData.js
@@ -1,17 +1,26 @@
 // exports.handler = async event => {
 
 // };
+const Responses = require('./API_Responses');
+
 const axios = require('axios');
-const header = 'fa9451f43414ff45a8b35f81737326b8';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=Melbourne&appid=${header}`;
-exports.handler = function(event, context, callback) {
-    axios.get(url).then(res => {
-        console.log(res.data)
-        callback(null, {
-            'statusCode': 200,
-            'body': res.data,
-        })
-    }).catch(() => {
-        console.log("->")
-    });
+const apiKey = 'fa9451f43414ff45a8b35f81737326b8';
+exports.handler = async event => {
+    console.log('event', event);
+
+    if (!event.pathParameters || !event.pathParameters.CITY) {
+        // failed without an CITY
+        return Responses._400({ message: 'missing the CITY from the path' });
+    }
+
+    let CITY = event.pathParameters.CITY;
+
+    if (!!event.pathParameters.CITY) {
+        // return the data
+        return Responses._200(data[CITY]);
+    }
+
+
+    //failed as CITY not in the data
+    return Responses._400({ message: 'no CITY in data' });
 };
